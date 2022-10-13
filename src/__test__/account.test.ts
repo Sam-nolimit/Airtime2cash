8cf6ff7931c29c1b3ecdc28eb04730d4b640dc41
Temitope Adejolu <tadejolu@gmail.com>
2022-10-13 12:23:16 +0100

    ci/cd-second-implementation

--- src/__test__/account.test.ts
+++
@@ -1,7 +0,0 @@
-function addNumber(input: number[]): number {
-  return input.reduce((acc, cur) => acc + cur, 0);
-}
-
-test("Add an array of numbers together using reduce function", () => {
-  expect(addNumber([10, 9])).toEqual(19);
-})
